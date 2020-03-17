ee3e492d16df2b68b4a1fe2bf8313f56a83ed2af
Caoxw2019 <50392950+Caoxw2019@users.noreply.github.com>
2020-03-18 00:02:46 +0800

    Initial commit

---
+++ src/main/java/life/gzhmt/forums/forums/ForumsApplication.java
@@ -0,0 +1,13 @@
+package life.gzhmt.forums.forums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/gzhmt/forums/forums/ForumsApplicationTests.java
@@ -0,0 +1,13 @@
+package life.gzhmt.forums.forums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
