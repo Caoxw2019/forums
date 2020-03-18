7387fbb04de3188f61a1451ba055bf8a1951d9da
Caoxw2019 <50392950+Caoxw2019@users.noreply.github.com>
2020-03-18 22:30:16 +0800

    变量-->公共属性
    以方便日后项目的扩展、修改，不至于修改源代码 只修改application.properties
    @value注入

--- src/main/java/life/gzhmt/forums/forums/controller/AuthoizeController.java
+++ src/main/java/life/gzhmt/forums/forums/controller/AuthoizeController.java
@@ -5,6 +5,7 @@ import life.gzhmt.forums.forums.dto.AccessTokenDTO;
 import life.gzhmt.forums.forums.dto.GithubUser;
 import life.gzhmt.forums.forums.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,25 @@ public class AuthoizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    //@value作用是在配置文件中加载值到赋值给下面
+    @Value("${github.client.id}")
+    private String clienId;
+
+    @Value("${github.client.secret}")
+    private String clienSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("7c6566bb77526da2203e");
-        accessTokenDTO.setClient_secret("c597365418db0f0a1b82cb8804dbe377f5080e15");
+        accessTokenDTO.setClient_id(clienId);
+        accessTokenDTO.setClient_secret(clienSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
